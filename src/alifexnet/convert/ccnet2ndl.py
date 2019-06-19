cba6d2a1f73cf919bfb2d47533faeb5f158ea0a4
NickleDave <nickledave@users.noreply.github.com>
2019-06-19 17:37:30 -0400

    rewrite convert.ccnet2ndl.conv2d, add ccnet2ndl.pool
    - conv2d now converts a conv layer from a Krizhevzky
      cudaconvnet .pkl file's ['model_state']['layers'] dictionary into a
      different dictionary that can be used as keyword arguments when
      creating an instance of a Conv2DNode.
    - added `pool` function, which does the same thing for a pooling layer

--- src/alifexnet/convert/ccnet2ndl.py
+++ src/alifexnet/convert/ccnet2ndl.py
@@ -1,13 +1,21 @@
 """
-convert layers from nengo_extras.cuda_convnet to NengoDL TensorNodes
+convert layers from nengo_extras.cuda_convnet to config for NengoDL TensorNodes
 """
 import numpy as np
 
-from .. import layernodes
+
+CONV2D_PARAMS = ['input_size',
+                 'n_channels',
+                 'n_filters',
+                 'kernel_size',
+                 'strides',
+                 'padding',
+                 'weights',
+                 'biases']
 
 
-def conv2d(conv2d_layer, **conv2d_kwargs):
-    """converts CudaConvNet conv2d to an alifexnet LayerNode,
+def conv2d(conv2d_layer):
+    """converts CudaConvNet conv2d to configuration for Conv2DNode
     which can be part of a net run in a nengo-dl simulator
     using a nengo_dl.TensorNode
 
@@ -16,50 +24,89 @@ def conv2d(conv2d_layer, **conv2d_kwargs):
     conv2d_layer : dict
         a conv2d layer from a "model_state" dictionary, loaded from
         a .pkl file created by Alex Krizhevsky's cuda-convnet library
-    conv2d_kwargs : kwargs
-        any valid keyword arguments to a tensorflow.keras.layers.Conv2d
-        layer that are not already defined in conv2d_layer, i.e., any
-        arguments besides filters, kernel_size, strides, and padding.
 
     Returns
     -------
-    conv2d_node : alifexnet.layernodes.Conv2DNode
-        where con2d_node.conv2d will have same parameters as the
-        layer from the CudaConvNet (number of filters, filter size,
-        padding, etc.), and will load the weights and biases from
-        that layer using its `post_build` method
+    conv2d_config : dict
+        with the following keyword arguments :
+            n_channels : int
+            input_size : int
+            n_filters : int
+            kernel_size : int
+            strides : int
+            padding : int
 
     Notes
     -----
-    Currently only 'valid' padding is implemented
+    Use the load_model_pickle function from this library to load model.pkl
+    files that contain CudaConvnet configs, because that function does some
+    extra clean-up
     """
     # unpack params.
-    # Note all are one-element arrays, we index to get just scalar value
-    n_channels = conv2d_layer['channels'][0]
-    input_size = conv2d_layer['imgSize'][0]
+    n_channels = conv2d_layer['channels']
+    input_size = conv2d_layer['imgSize']
     n_filters = conv2d_layer['filters']
-    kernel_size = conv2d_layer['filterSize'][0]
-    strides = conv2d_layer['stride'][0]
-    padding = conv2d_layer['padding'][0]
-    if padding == 0:
+    kernel_size = conv2d_layer['filterSize']
+    strides = conv2d_layer['stride']
+
+    if conv2d_layer['padding'] == 0:
         padding = 'valid'
     else:
-        raise NotImplementedError('only valid (zero) padding implemented')
+        raise ValueError('only valid (zero) padding implemented')
 
     # get weights + biases to load in post-build method
-    weights = conv2d_layer['weights'][0].reshape(n_channels,
-                                                 kernel_size,
-                                                 kernel_size,
-                                                 n_filters)
+    weights = conv2d_layer['weights'].reshape(n_channels,
+                                              kernel_size,
+                                              kernel_size,
+                                              n_filters)
     weights = np.rollaxis(weights, axis=-1, start=0)
     biases = conv2d_layer['biases']
 
-    return layernodes.Conv2DNode(input_size,
-                                 n_channels,
-                                 filters=n_filters,
-                                 kernel_size=kernel_size,
-                                 strides=strides,
-                                 padding=padding,
-                                 weights=weights,
-                                 biases=biases,
-                                 **conv2d_kwargs)
+    conv2d_config = dict(zip(
+        CONV2D_PARAMS,
+        [input_size, n_channels, n_filters, kernel_size, strides,
+         padding, weights, biases]
+    ))
+    return conv2d_config
+
+
+POOL_PARAMS = ['input_size',
+               'n_channels',
+               'pool_size',
+               'strides',
+               'padding',
+               'kind']
+
+
+def pool(pool_layer):
+    """
+    
+    Parameters
+    ----------
+    pool_layer : dict
+        a pool layer from a "model_state" dictionary, loaded from
+        a .pkl file created by Alex Krizhevsky's cuda-convnet library
+
+    Returns
+    -------
+    pool_config : dict
+        with the following keyword arguments :
+            input_size : int
+            n_channels : int
+            pool_size : int
+            strides : int
+            padding : int
+            kind : str
+    """
+    n_channels = pool_layer['channels']
+    input_size = pool_layer['imgSize']
+    pool_size = pool_layer['sizeX']
+    strides = pool_layer['stride']
+    padding = 'full'
+    kind = pool_layer['pool']
+
+    pool_config = dict(zip(
+        POOL_PARAMS,
+        [input_size, n_channels, pool_size, strides, padding, kind]
+    ))
+    return pool_config
