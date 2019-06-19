374c876cc581034ba8f750dc3e1575e7bb94e77d
NickleDave <nickledave@users.noreply.github.com>
2019-06-19 17:41:14 -0400

    load_model_pickle in utils.py does extra clean-up of model
    - uses dictionary comprehension to go through any parameters of a 'layer'
      and unpack one-item lists so there's not a bunch of
      'config_dict['param'] = cuda_convnet_layer['param'][0]'
      in conversion functions

--- src/alifexnet/convert/utils.py
+++ src/alifexnet/convert/utils.py
@@ -1,3 +1,42 @@
+"""
+adapted from https://github.com/nengo/nengo-extras
+"""
+import os
+import pickle
+
+import numpy as np
+
+
+def pickle_load(file, *args, **kwargs):
+    kwargs.setdefault('encoding', 'latin1')
+    return pickle.load(file, *args, **kwargs)
+
+
+def load_model_pickle(loadfile):
+    loadfile = os.path.expanduser(loadfile)
+    with open(loadfile, 'rb') as f:
+        model = pickle_load(f)
+
+    def _scalar_or_arr(val):
+        """return scalar value if val is a one-element array,
+        else return array"""
+        if type(val) == list:
+            if len(val) == 1:
+                return val[0]
+            else:
+                return val
+        else:
+            return val
+
+    new_layers = {}
+    for layer_name, layer_config in model['model_state']['layers'].items():
+        new_layers[layer_name] = {
+            key: _scalar_or_arr(val)
+            for key, val in layer_config.items()
+        }
+
+    model['model_state']['layers'] = new_layers
+    return model
 
 
 def layer_depths(layers):
